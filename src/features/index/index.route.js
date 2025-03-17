03baaad32365785f18c03657fef11f6f42ec2298
devanshdeveloper <65781793+devanshdeveloper@users.noreply.github.com>
2025-03-17 12:03:24 +0530

    commit

--- src/features/index/index.route.js
+++ src/features/index/index.route.js
@@ -4,9 +4,7 @@ const ResponseHelper = require("../../helpers/ResponseHelper");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  return new ResponseHelper(res)
-    .success({ status: "EduManage API is running" })
-    .send();
+  return new ResponseHelper(res).message("EduManage API is running").send();
 });
 
 module.exports = router;
